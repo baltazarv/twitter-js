2f06397b4f6224cf1b2c2c3ba8a6e568c576ceb5
Jeremy Grubard <jgrubard@gmail.com>
2018-01-21 16:40:37 -0500

    commit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+});
+
 app.get('/', (req, res) => {
   res.send('hello');
 });
 
+app.get('/news', (req, res) => {
+  res.send('News');
+});
 
 app.listen(3000);
